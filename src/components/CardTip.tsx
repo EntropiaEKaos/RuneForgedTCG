0586bb40e273a5837d0c0f28cb307dd50f6cf389
Wilton C <131200742+EntropiaEKaos@users.noreply.github.com>
2026-08-29 13:15:28 -0300

    feat(alpha): pass rendered card context into tooltip

--- src/components/CardTip.tsx
+++ src/components/CardTip.tsx
@@ -5,9 +5,9 @@ import CardInfo from "./CardInfo";
 import Tooltip from "./Tooltip";
 
 export default function CardTip(props: CardViewProps) {
-  const { defId, unit, state } = props;
+  const { defId, definition, unit, state, costOverride } = props;
   return (
-    <Tooltip content={<CardInfo defId={defId} unit={unit} state={state} />}>
+    <Tooltip content={<CardInfo defId={defId} definition={definition} unit={unit} state={state} costOverride={costOverride} />} panelWidth={420}>
       <span data-unit-id={unit?.instanceId} className="inline-block">
         <CardView {...props} />
       </span>
